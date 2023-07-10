daab92f338050066f87d0a3fc2e52e817381aed3
Florence Selvaraj <florencebf17@gmail.com>
2023-07-10 10:23:54 +0000

    Updated karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -9,6 +9,7 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-firefox-launcher'),
+      require('karma-junit-reporter'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -33,7 +34,7 @@ module.exports = function (config) {
       file: 'code-coverage.xml',
       reporters: [ { type: 'cobertura' } ]
     },
-    reporters: ['progress', 'coverage', 'kjhtml'],
+    reporters: ['progress', 'coverage', 'junit'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
